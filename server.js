47bcfc0814b03df703f7563b3f03587676a9990c
zaeem-socioladder <zaeem.farooqui@socioladder.com>
2022-01-29 20:57:56 +0530

    host

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 let broadcaster;
-const port = 4000;
+var port = process.env.PORT || 4000;
 
 const http = require("http");
 const server = http.createServer(app);
